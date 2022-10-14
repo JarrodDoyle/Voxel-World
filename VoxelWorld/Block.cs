5a35cb7957bcc31791d538d0cc9e522c6cef736e
jarrod <thejarroddoyle@gmail.com>
2022-10-14 15:42:27 +0100

    Reduced amount of memory used by each block

--- VoxelWorld/Block.cs
+++ VoxelWorld/Block.cs
@@ -4,14 +4,17 @@ namespace VoxelWorld;
 
 public class Block
 {
-    public Vector3 Position { get; }
+    private readonly uint _position;
+    private readonly uint _color;
+
+    public Vector3 Position => new((_position >> 16) & 0xFF, (_position >> 8) & 0xFF, _position & 0xFF);
     public BlockType BlockType { get; }
-    public Vector4 Color { get; }
+    public Vector4 Color => new((_color >> 24) & 0xFF, (_color >> 16) & 0xFF, (_color >> 8) & 0xFF, _color & 0xFF);
 
     public Block(Vector3 position, BlockType blockType, Vector4 color)
     {
-        Position = position;
+        _position = ((uint) position.X << 16) + ((uint) position.Y << 8) + (uint) position.Z;
         BlockType = blockType;
-        Color = color;
+        _color = ((uint) color.X << 24) + ((uint) color.Y << 16) + ((uint) color.Z << 8) + (uint) color.W;
     }
 }
