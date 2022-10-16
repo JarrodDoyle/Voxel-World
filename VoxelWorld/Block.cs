5e00baedb09ac3653d0ec16753d0aae3e4901c1a
jarrod <thejarroddoyle@gmail.com>
2022-10-16 20:38:59 +0100

    Reduce block memory footprint to 5 bytes (from 8) BlockType is 4 bytes, but if we'll only have a low amount of materials we can just cast it so something smaller like a byte. Importantly we need to use the StructLayout attribute with a packing of 1 otherwise blocks will be padded to 8 bytes.

--- VoxelWorld/Block.cs
+++ VoxelWorld/Block.cs
@@ -1,17 +1,20 @@
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace VoxelWorld;
 
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
 public readonly struct Block
 {
     private readonly uint _color;
+    private readonly byte _blockType;
 
-    public BlockType BlockType { get; }
+    public BlockType BlockType => (BlockType) _blockType;
     public Vector4 Color => new((_color >> 24) & 0xFF, (_color >> 16) & 0xFF, (_color >> 8) & 0xFF, _color & 0xFF);
 
     public Block(BlockType blockType, Vector4 color)
     {
-        BlockType = blockType;
+        _blockType = (byte) blockType;
         _color = ((uint) color.X << 24) + ((uint) color.Y << 16) + ((uint) color.Z << 8) + (uint) color.W;
     }
 }
