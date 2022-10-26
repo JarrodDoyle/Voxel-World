a92668a9134df639eb7ce775bf989ced89598c47
jarrod <thejarroddoyle@gmail.com>
2022-10-26 16:30:19 +0100

    Reduce per-voxel memory usage from 5 bytes to 2 bytes This comes at a loss of colour range. We go from 2^32 colours to using a fixed world palette of 2^8 colours.

--- VoxelWorld/Block.cs
+++ VoxelWorld/Block.cs
@@ -6,15 +6,15 @@ namespace VoxelWorld;
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public readonly struct Block
 {
-    private readonly uint _color;
+    private readonly byte _color;
     private readonly byte _blockType;
 
     public BlockType BlockType => (BlockType) _blockType;
-    public Vector4 Color => new((_color >> 24) & 0xFF, (_color >> 16) & 0xFF, (_color >> 8) & 0xFF, _color & 0xFF);
+    public Vector4 Color => World.Palette[_color];
 
     public Block(BlockType blockType, Vector4 color)
     {
         _blockType = (byte) blockType;
-        _color = ((uint) color.X << 24) + ((uint) color.Y << 16) + ((uint) color.Z << 8) + (uint) color.W;
+        _color = (byte) ((color.X + color.Y + color.Z) / 12f);
     }
 }
